0f7ead9e5a161b911496a42e308f2f2cb1e709dd
maxkram <max.kramer@live.com>
2018-02-28 08:52:27 -0500

    first_one

---
+++ src/Components/App.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class App extends Component{
+  componentDidMount(){
+    console.log('React Running....');
+  }
+
+  render(){
+    return (
+      <div>
+        My App
+      </div>
+    );
+  }
+}
+
+export default App;
